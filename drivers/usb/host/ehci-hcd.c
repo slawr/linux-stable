8178f008220282f85f2bd6131eaca0c88bff97e1
Phil Edworthy <phil.edworthy@renesas.com>
2012-06-28 15:09:35 +0100

    usb: host: Add glue code for R-Car OHCI/EHCI platform drivers on r8a7779
    Signed-off-by: Phil Edworthy <phil.edworthy@renesas.com>

--- drivers/usb/host/ehci-hcd.c
+++ drivers/usb/host/ehci-hcd.c
@@ -1385,6 +1385,11 @@ MODULE_LICENSE ("GPL");
 #define PLATFORM_DRIVER		ehci_platform_driver
 #endif
 
+#ifdef CONFIG_ARCH_R8A7779
+#include "ehci-rcar.c"
+#define	PLATFORM_DRIVER		rcar_ehci_driver
+#endif
+
 #if !defined(PCI_DRIVER) && !defined(PLATFORM_DRIVER) && \
     !defined(PS3_SYSTEM_BUS_DRIVER) && !defined(OF_PLATFORM_DRIVER) && \
     !defined(XILINX_OF_PLATFORM_DRIVER)
