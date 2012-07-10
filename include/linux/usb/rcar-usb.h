98286649cb324337042e3f2c2d0e74ddc3aba9de
Phil Edworthy <phil.edworthy@renesas.com>
2012-07-10 09:56:50 +0100

    usb: host: Add USB host OHCI controller for R-Car
    Signed-off-by: Phil Edworthy <phil.edworthy@renesas.com>

---
+++ include/linux/usb/rcar-usb.h
@@ -0,0 +1,27 @@
+/*
+ * include/linux/usb/rcar-usb.h
+ *
+ * Copyright (C) 2011-2012 Renesas Electronics Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __LINUX_USB_RCAR_USB_H
+#define __LINUX_USB_RCAR_USB_H
+
+struct rcar_usb_info {
+	int port_offset;
+};
+
+#endif /* __LINUX_USB_RCAR_USB_H */
