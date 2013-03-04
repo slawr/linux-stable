a39472850c5e269f743efb5ef04c9cdd005b5ce8
Phil Edworthy <phil.edworthy@renesas.com>
2013-03-04 09:53:56 +0000

    rcarvin: Add ability to limit the MBUS format used
    Currently, the VIN driver reports all formats supported by the SoC
    camera. When the VIN driver and SoC camera support a number of
    common formats, we need to be able to specify which one to use.

--- include/media/rcarvin.h
+++ include/media/rcarvin.h
@@ -3,13 +3,12 @@
 
 #include <media/soc_camera.h>
 
-#define RCAR_VIN_FLAG_USE_8BIT_BUS	(1 << 0) /* use  8bit bus width */
-#define RCAR_VIN_FLAG_USE_16BIT_BUS	(1 << 1) /* use 16bit bus width */
-#define RCAR_VIN_FLAG_HSYNC_LOW		(1 << 2) /* default High if possible */
-#define RCAR_VIN_FLAG_VSYNC_LOW		(1 << 3) /* default High if possible */
+#define RCAR_VIN_FLAG_HSYNC_LOW		(1 << 0) /* default High if possible */
+#define RCAR_VIN_FLAG_VSYNC_LOW		(1 << 1) /* default High if possible */
 
 struct rcar_vin_info {
 	unsigned long flags;
+	enum v4l2_mbus_pixelcode format;
 };
 
 #endif /* __ASM_RCAR_VIN_H__ */
